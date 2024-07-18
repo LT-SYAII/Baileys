5ae4ee9e793e66fbedbd0441479e953082c20d63
BANG SYAII <162920276+LT-SYAII@users.noreply.github.com>
2024-07-19 04:36:12 +0700

    Update index.ts

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -16,6 +16,14 @@ import { SocketConfig } from './Socket'
 
 export type UserFacingSocketConfig = Partial<SocketConfig> & { auth: AuthenticationState }
 
+export type BrowsersMap = {
+    ubuntu(browser: string): [string, string, string]
+    macOS(browser: string): [string, string, string]
+    baileys(browser: string): [string, string, string]
+    windows(browser: string): [string, string, string]
+    appropriate(browser: string): [string, string, string]
+}
+
 export enum DisconnectReason {
     connectionClosed = 428,
     connectionLost = 408,
@@ -54,6 +62,6 @@ export type WABusinessProfile = {
     category?: string
     wid?: string
     address?: string
-}
 
+    }
 export type CurveKeyPair = { private: Uint8Array, public: Uint8Array }
